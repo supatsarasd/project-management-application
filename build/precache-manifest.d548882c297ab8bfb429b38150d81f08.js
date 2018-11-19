bfb3857ecc383ef10c00712056406ef2762fcd62
supatsarasd <45132578+supatsarasd@users.noreply.github.com>
2018-11-19 16:01:05 +0000

    Add files via upload

---
+++ build/precache-manifest.d548882c297ab8bfb429b38150d81f08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "de6a8b509b79653daeed",
+    "url": "/static/js/main.de6a8b50.chunk.js"
+  },
+  {
+    "revision": "5adc709eaaeecca72359",
+    "url": "/static/js/1.5adc709e.chunk.js"
+  },
+  {
+    "revision": "de6a8b509b79653daeed",
+    "url": "/static/css/main.025ef3f6.chunk.css"
+  },
+  {
+    "revision": "d45b3c2b0099d6c4e26b241a59dfd460",
+    "url": "/index.html"
+  }
+];
